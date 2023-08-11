572bb42d45734590238f5fc920b9bac106fd7f04
Adetokunbo Ogunnoiki <adetokunboogunnoiki28@gmail.com>
2023-08-11 22:58:42 +0100

    a step further

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+" use client";
 
+import { useState, useEffect } from "react";
+import { getProviders, signIn } from "next-auth/react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signinUrl: string;
+  callbackUrl: string;
+  signinUrlParams?: Record<string, string> | null;
+};
+
+type Providers = Record<string, Provider>;
 const AuthProvider = () => {
-  return (
-    <>
-    AuthProvider
-    </>
-  )
-}
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProviders = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    };
+    fetchProviders();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, i) => (
+          <button key={i}>{provider.id}</button>
+        ))}
+      </div>
+    );
+  }
+};
 
 export default AuthProvider;
---
+++ lib/session.ts
@@ -0,0 +1,40 @@
+import { getServerSession } from "next-auth/next";
+import { NextAuthOptions, User } from "next-auth";
+import {AdapterUser } from "next-auth/adapters";
+import GoogleProvider from "next-auth/providers/google";
+import { JWT } from "next-auth/jwt";
+import jsonwebtoken from "jsonwebtoken";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  // jwt: {
+  //     encode: async ({ secret, token}) => {
+
+  //     },
+  //     decode: async ({ secret, token }) => {
+
+  //     }
+  // },
+  theme: {
+    logo: "/logo.png",
+    colorScheme: "light",
+  },
+  callbacks: {
+    async session({ session }) {
+      return session;
+    },
+
+    async signIn({ user }: { user: User | AdapterUser }) {
+      try {
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
+};
